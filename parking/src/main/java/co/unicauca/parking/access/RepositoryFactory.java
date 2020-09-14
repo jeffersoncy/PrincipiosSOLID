500275fd0926bc8506eb36a87ffeffadd9c73a0c
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2020-09-14 13:18:37 -0500

    more

--- parking/src/main/java/co/unicauca/parking/access/RepositoryFactory.java
+++ parking/src/main/java/co/unicauca/parking/access/RepositoryFactory.java
@@ -5,10 +5,49 @@
  */
 package co.unicauca.parking.access;
 
+
+import co.unicauca.solid.dip.domain.access.IVehicleRepository;
+import co.unicauca.solid.dip.domain.access.VehicleRepository;
+
 /**
  *
  * @author Personal
  */
 public class RepositoryFactory {
+    private static RepositoryFactory instance;
     
+    private RepositoryFactory() {
+    }
+
+    /**
+     * Clase singleton
+     *
+     * @return
+     */
+    public static RepositoryFactory getInstance() {
+
+        if (instance == null) {
+            instance = new RepositoryFactory();
+        }
+        return instance;
+
+        /**
+     * Método que crea una instancia concreta de la jerarquia IProductRepository
+     *
+     * @param type cadena que indica qué tipo de clase hija debe instanciar
+     * @return una clase hija de la abstracción IProductRepository
+     */
+    public IVehicleRepository getRepository(String type) {
+
+        IVehicleRepository result = null;
+
+        switch (type) {
+            case "default":
+                result = new VehicleRepository();
+                break;
+        }
+
+        return result;
+
+    }
 }
