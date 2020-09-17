cde37e741837c864f86e7ef361542eafffc1f15d
jeffersoncy <48133501+jeffersoncy@users.noreply.github.com>
2020-09-16 22:16:32 -0500

    carros ok

--- parking/src/main/java/co/unicauca/parking/domain/TruckPartingCost.java
+++ parking/src/main/java/co/unicauca/parking/domain/TruckPartingCost.java
@@ -6,6 +6,7 @@
 package co.unicauca.parking.domain;
 
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -15,7 +16,27 @@ public class TruckPartingCost implements IParkingCost {
 
     @Override
     public double calculateCost(Date input, Date output) {
-        return 0;
+       long diferencia = output.getTime()-input.getTime();
+       int minutos = (int) TimeUnit.MILLISECONDS.toMinutes(diferencia);
+       long horas = TimeUnit.MILLISECONDS.toHours(diferencia);
+       minutos = (int) (minutos - TimeUnit.HOURS.toMinutes(horas));
+       double cuentaCobro = 0;
+       double cuentaCobromin = 0;
+       if(horas < 12)
+       {
+           cuentaCobro = 10000;
+       }
+       else
+       {
+           cuentaCobro = 2000;
+           cuentaCobromin = ((1000 * (minutos * 100 / 60))/100);
+           for (int i = 0; i < horas-1; i++)
+           {
+               cuentaCobro = cuentaCobro + 1000;
+           }
+       }
+       double cuentatotal = (cuentaCobro+cuentaCobromin)*0.01;
+       return Math.ceil(cuentatotal)*100;
     }
     
 }
